cecc5f85270b61d5571d6e5f8b9a8e99ed213de4
TheNuee <43197392+TheNuee@users.noreply.github.com>
2019-02-28 18:16:12 +0700

    Add files via upload

---
+++ latihan4.php
@@ -0,0 +1,50 @@
+<html>
+    <head>
+        <title>Set Session</title>
+    </head>
+    <body>
+        <?php
+        // Inisialisasi data session
+        session_start();
+        // Set session jika belum ada
+        if (!isset($_SESSION['test'])) {
+            $_SESSION['test'] = 'value';
+        } else {
+            echo 'Session di-set <br />';
+        // Mencetak nilai session test
+            echo 'Nilai: ' . $_SESSION['test'] . '<br />';
+        // Mencetak semua elemen session
+            print_r($_SESSION);
+        }
+        ?>
+    <p>Tekan Refresh (F5);</p>
+    </body>
+</html>
+
+o Menghapus Session
+Untuk menghapus data session, Anda bisa menggunakan konstruksi bahasa unset() atau
+fungsi session_destroy().
+<html>
+<head>
+<title>Hapus Session</title>
+</head>
+<body>
+<?php
+// Inisialisasi data session
+session_start();
+// Set session jika belum ada
+if (isset($_SESSION['test'])) {
+// Hapus session test
+
+unset($_SESSION['test']);
+echo 'session dihapus';
+} else {
+echo 'unset';
+// Mencetak semua elemen session
+print_r($_SESSION);
+}
+?>
+<p>
+Tekan Refresh (F5);
+</body>
+</html>
